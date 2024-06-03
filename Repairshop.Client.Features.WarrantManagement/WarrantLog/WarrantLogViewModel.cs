b92f0162cc18bfb62c21817674a540a9d8b3fb0a
Boris Horvat-Korent <boris.horvat.korent@outlook.com>
2024-06-03 21:06:34 +0200

    Added warrant log filter

--- Repairshop.Client.Features.WarrantManagement/WarrantLog/WarrantLogViewModel.cs
+++ Repairshop.Client.Features.WarrantManagement/WarrantLog/WarrantLogViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Repairshop.Client.Common.Interfaces;
 using Repairshop.Client.Common.Navigation;
 using Repairshop.Client.Features.WarrantManagement.Interfaces;
@@ -10,6 +11,15 @@ public partial class WarrantLogViewModel
 {
     private readonly IWarrantLogService _warrantLogService;
     private readonly ILoadingIndicatorService _loadingIndicatorService;
+
+    [ObservableProperty]
+    private bool _isFilterVisible;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(LogEntries))]
+    private string? _technicianNameFilter;
+
+    private string? _warrantNumbeFilter;
 
     private IReadOnlyCollection<WarrantLogEntryViewModel> _logEntries
         = new List<WarrantLogEntryViewModel>();
@@ -24,13 +34,34 @@ public partial class WarrantLogViewModel
 
     public IReadOnlyCollection<WarrantLogEntryViewModel> LogEntries
     {
-        get => _logEntries.OrderByDescending(x => x.EventTime).ToList();
+        get => _logEntries
+            .Where(x => string.IsNullOrEmpty(TechnicianNameFilter) 
+                || x.TechnicianName?.ToLower().Contains(TechnicianNameFilter.ToLower()) == true)
+            .Where(x => string.IsNullOrEmpty(WarrantNumberFilter)
+                || x.WarrantNumber == int.Parse(WarrantNumberFilter))
+            .OrderByDescending(x => x.EventTime)
+            .ToList();
         set => SetProperty(ref _logEntries, value);
+    }
+
+    public string? WarrantNumberFilter
+    {
+        get => _warrantNumbeFilter;
+        set
+        {
+            if (!int.TryParse(value, out _) && !string.IsNullOrEmpty(value)) return;
+
+            SetProperty(ref _warrantNumbeFilter, string.IsNullOrEmpty(value) ? null : value);
+            OnPropertyChanged(nameof(LogEntries));
+        }
     }
 
     [RelayCommand]
     private Task OnLoaded() =>
         _loadingIndicatorService.ShowLoadingIndicatorForAction(LoadWarrantLog);
+
+    [RelayCommand]
+    private void ShowFilters() => IsFilterVisible = true;
 
     private async Task LoadWarrantLog()
     {
